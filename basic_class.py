caa4019d2e4e67fdb05a6fc2bd968b1fdd19eaef
Ganesh Chandrasekaran <gchandra@gmail.com>
2024-02-21 15:36:50 -0500

    Updates to Classes

--- basic_class.py
+++ basic_class.py
@@ -6,6 +6,9 @@ class Dog:
     def bark(self):
         print(f"{self.name} says woof! and its {self.age} years old")
 
+
 my_dog = Dog("Buddy", 2)
+your_dog = Dog("Brady", 3)
 
 my_dog.bark()
+your_dog.bark()
