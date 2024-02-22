9db33baffc79e1483760073aa0cabfd4a46c62de
Ganesh Chandrasekaran <gchandra@gmail.com>
2024-02-22 16:41:44 -0500

    updates

--- advance_inheritence_class.py
+++ advance_inheritence_class.py
@@ -1,4 +1,9 @@
 ## Demonstrates Inheritence and Function Overriding
+## Dog IS A Animal
+## Cat IS A Animal
+## Employee IS A Person
+## Student IS A Person
+
 
 class Animal:
     def __init__(self, name):
